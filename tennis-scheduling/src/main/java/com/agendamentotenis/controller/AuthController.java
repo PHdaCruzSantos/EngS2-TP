cdad1072d4f878905dc20bf30a2bf0d2c9b1a438
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2025-02-21 17:21:32 -0300

    feat: Auth controller

---
+++ tennis-scheduling/src/main/java/com/agendamentotenis/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.agendamentotenis.controller;
+
+import com.agendamentotenis.security.JwtConfig;
+import com.agendamentotenis.model.User;
+import com.agendamentotenis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginUser) {
+        return userService.findByEmail(loginUser.getEmail())
+                .filter(user -> passwordEncoder.matches(loginUser.getPassword(), user.getPassword()))
+                .map(user -> {
+                    String token = jwtConfig.generateToken(user.getEmail());
+                    Map<String, Object> response = new HashMap<>();
+                    response.put("token", token);
+                    response.put("user", user);
+                    return ResponseEntity.ok(response);
+                })
+                .orElseGet(() -> {
+                    Map<String, Object> errorResponse = new HashMap<>();
+                    errorResponse.put("error", "Credenciais inválidas");
+                    return ResponseEntity.badRequest().body(errorResponse);
+                });
+    }
+}
