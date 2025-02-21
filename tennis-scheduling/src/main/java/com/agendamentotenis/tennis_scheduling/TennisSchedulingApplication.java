6f23ce1ac6573338941d21271566ebc45960ee4b
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2025-02-21 17:11:07 -0300

    fix: Dependecies

---
+++ tennis-scheduling/src/main/java/com/agendamentotenis/tennis_scheduling/TennisSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.agendamentotenis.tennis_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisSchedulingApplication.class, args);
+	}
+
+}
---
+++ tennis-scheduling/src/test/java/com/agendamentotenis/tennis_scheduling/TennisSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamentotenis.tennis_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
