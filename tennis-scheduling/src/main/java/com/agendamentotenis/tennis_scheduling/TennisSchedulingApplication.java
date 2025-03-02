e29cf486ac7f5bef520a720387709a5faef896b1
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2025-03-02 19:50:29 -0300

    config: update application yml

--- tennis-scheduling/src/main/java/com/agendamentotenis/tennis_scheduling/TennisSchedulingApplication.java
+++ tennis-scheduling/src/main/java/com/agendamentotenis/tennis_scheduling/TennisSchedulingApplication.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.agendamentotenis" })
 public class TennisSchedulingApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set environment variables for MongoDB connection
+		System.setProperty("MONGODB_USERNAME", dotenv.get("MONGODB_USERNAME"));
+		System.setProperty("MONGODB_PASSWORD", dotenv.get("MONGODB_PASSWORD"));
+		System.setProperty("MONGODB_CLUSTER", dotenv.get("MONGODB_CLUSTER"));
+		System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
+
 		SpringApplication.run(TennisSchedulingApplication.class, args);
 	}
 }
