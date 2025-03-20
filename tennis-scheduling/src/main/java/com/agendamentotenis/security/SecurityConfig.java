aae39f259e64abab1b52ceca7e7a7e31d1a8ea49
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2025-03-20 14:59:31 -0300

    config: jwt config

--- tennis-scheduling/src/main/java/com/agendamentotenis/security/SecurityConfig.java
+++ tennis-scheduling/src/main/java/com/agendamentotenis/security/SecurityConfig.java
@@ -11,9 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,14 +27,20 @@ public class SecurityConfig {
     private static final String USER_ID_PATH = "/api/users/{id}";
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter) throws Exception {
         http
                 .authorizeHttpRequests(authorizeConfig -> authorizeConfig
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/mathces").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/matches").authenticated()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable) // Desabilita CSRF para APIs stateless
-                .cors(Customizer.withDefaults()); // Habilita CORS
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Não cria sessão
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class); // Adiciona o filtro JWT
 
         return http.build();
     }
